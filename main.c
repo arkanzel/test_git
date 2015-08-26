0a37795c5e629edc8bbe3ee3bc16d59728c81292
arkanzel <arkanzelx@gmail.com>
2015-08-26 13:15:26 +0200

    Ajout d'une fonction capitalize sur le main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+char *ft_capitalize (char *str)
+{
+	str[0] -= ('a' - 'A');
+	return str;
+}
+
+
 int main(void)
 {
 	printf("Modifié depuis github\n");
